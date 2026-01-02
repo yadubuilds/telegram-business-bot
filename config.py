7a434f3b52da0e9cb55cbd53f406f76883eaf1bc
yadubuilds <mail@yadusins.com>
2026-01-02 07:33:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_ID = 23250949        # from my.telegram.org
+API_HASH = "25ad48c711d987870524b0bb9d8915be"
+BOT_TOKEN = "8163553294:AAGA9i9zz-YSmh3HYAv1BvuSTrF9UzWIqQo"
+
+ADMINS = [7358540435]   # your Telegram ID
+
+# Subscription plans (days)
+PLANS = {
+    "basic": 7,
+    "pro": 30,
+    "vip": 90
+}
